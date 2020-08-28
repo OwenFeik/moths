25d191fec7b373dafd810fbc34144d8756405011
OwenFeik <owenhfeik@gmail.com>
2020-08-28 14:28:09 +1000

    fix drag

--- physics.c
+++ physics.c
@@ -14,12 +14,13 @@
 #define DTHETA 0.1
 #define DVEL 3
 
-#define DRAG -0.5
+#define DRAG 1
+#define MIN_DRAG 0.1
 #define VELMAX 10
 
-float theta = 0;
-float v_x = 0, v_y = 0;
-float p_x = 0, p_y = 0;
+double theta = 0;
+double v_x = 0, v_y = 0;
+double p_x = 0, p_y = 0;
 
 float to_range(float val, float geq, float leq) {
     if (val >= leq)
@@ -39,45 +40,54 @@ void apply_rotation(float dir) {
 
 void apply_acceleration(float dir) {
     v_x += DVEL * dir * cos(theta);
-    v_x = to_range(v_x, -VELMAX, VELMAX);
     v_y += DVEL * dir * sin(theta);
-    v_y = to_range(v_y, -VELMAX, VELMAX);
 }
 
 void apply_drag() {
     int s_x = v_x > 0;
     int s_y = v_y > 0;
 
-    printf("vy: %.2f, vx: %.2f\n", v_y, v_x);
-
     double v_theta;
-    if (v_x > EPSILON && v_y > EPSILON)
-        v_theta = atan(abs(v_y) / abs(v_x));
-    if (v_y > EPSILON)
-        v_theta = PI / 2;
-    else {
+    double a_v_x = fabs(v_x);
+    double a_v_y = fabs(v_y);
+
+    if (a_v_x < EPSILON && a_v_y > EPSILON) {
+        v_theta = (PI / 2) * (s_y * 1 + !s_y * -1);
+        v_x = 0;
+    }
+    else if (a_v_y < EPSILON && a_v_x > EPSILON) {
+        v_theta = (PI * !s_x); 
+        v_y = 0;
+    }
+    else if (a_v_y < EPSILON && a_v_x < EPSILON) {
         v_x = v_y = 0;
         return;
     }
+    else {
+        v_theta = atan2(v_y, v_x);
+    }
 
-    printf("successfully assigned vt\n");
+    double x_drag = max(DRAG * pow(cos(v_theta), 2), MIN_DRAG); 
+    double y_drag = max(DRAG * pow(sin(v_theta), 2), MIN_DRAG);
 
-    float x_drag = DRAG * pow(cos(v_theta), 2); 
-    float y_drag = DRAG * pow(sin(v_theta), 2);
-
-    if (x_drag == x_drag)
+    if (v_x < 0)
         v_x += x_drag;
-    if (y_drag == y_drag)
+    else
+        v_x -= x_drag;
+
+    if (v_y < 0)
         v_y += y_drag;
-
-    printf("vt: %.2f, xd: %.2f, yd: %.2f, sum: %.2f\n", v_theta, x_drag, y_drag, x_drag + y_drag);
-
+    else
+        v_y -= y_drag;
 
     v_x = v_x * (s_x == v_x > 0);
     v_y = v_y * (s_y == v_y > 0);
 }
 
 void physics_tick() {
+    v_x = to_range(v_x, -VELMAX, VELMAX);
+    v_y = to_range(v_y, -VELMAX, VELMAX);
+
     apply_drag();
 
     p_x += v_x;
